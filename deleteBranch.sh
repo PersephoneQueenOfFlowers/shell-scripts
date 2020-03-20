7f3ab698819892673ff45d064b9241a08e1293f9
Seth Schoenfeld <sethschoenfeld@gmail.com>
2020-03-20 12:01:07 -0700

    Update deleteBranch.sh

--- deleteBranch.sh
+++ deleteBranch.sh
@@ -2,15 +2,15 @@
 #!/bin/bash
 # Usage: 
 # Create a directory called shell scripts at the same level as ddins. 
-# Place this file in the shell scripts directory,
+# Place this file in a directory adjacent to <my-git-directory> in need of cleaning.
 # Type the following text between the <> into terminal. <. deleteBranch.sh>
-# script changes directory into ddins, 
+# script changes directory into git directory, 
 # places all git branch names into a newly-generated file,
 # reads that file line-by-line, and 
 # deletes remotely, then locally, all branches which do not contain the phrases 'master' or 'dev'
 # deletes the file with branch names and returns cursor to shell-scripts directory. 
 
-cd ../ddins
+cd ../<my-git-directory>
 touch gitOut.txt
 git branch > gitOut.txt
 file=gitOut.txt
